6f6809c76fd52b42ea9025da5bb40a57604c4013
gumolotov <gustavo.levoratocorrea@hotmail.com>
2025-03-26 21:41:46 -0300

    Update server.js

--- API/server.js
+++ API/server.js
@@ -4,3 +4,21 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const comentariosRoutes = require('./src/routes/comentarios.routes');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('combined'));
+
+mongoose.connect('mongodb://mongo:27017/comentarios', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use('/comentarios', comentariosRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
