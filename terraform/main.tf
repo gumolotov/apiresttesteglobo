05d3dc9ad6483d1361afb169e3d8ce807b924090
unknown <gustavo.levoratocorrea@hotmail.com>
2025-03-27 09:35:40 -0300

    Alter role

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,17 +58,17 @@ resource "aws_iam_role" "restapi" {
   name = "restapi-ecs-task-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
       },
-    ]
-  })
+      "Action": "sts:AssumeRole"
+    }
+  ]
+})
 }
 
 resource "aws_iam_policy_attachment" "ecs_task_execution" {
