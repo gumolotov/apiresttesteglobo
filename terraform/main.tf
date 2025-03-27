1994db565d5155342f70888726d5a2a6f0cca97e
gumolotov <gustavo.levoratocorrea@hotmail.com>
2025-03-27 00:10:52 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,6 +49,22 @@ resource "aws_instance" "mongo_instance" {
                 sudo apt update
                 sudo apt install -y mongodb
                 sudo systemctl start mongodb
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-restapi-globo"
+  acl    = "private"
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
                 sudo systemctl enable mongodb
               EOF
 }
